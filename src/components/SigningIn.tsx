f9417a1d14bd5de2afe57b2a3a6f98fc4f7ddf2a
AnnaChernopyatova <anna.chernopyatova913@mail.ru>
2022-03-19 13:00:39 +0300

    Change signingIn component

--- src/components/SigningIn.tsx
+++ src/components/SigningIn.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './styles/signingIn.css';
 
 export default function SigningIn (){
-	const [email, setEmail] = React.useState<string>('');
-	const [password, setPassword] = React.useState<string>('');
+	const [email, setEmail] = React.useState<string>('...');
+	const [password, setPassword] = React.useState<string>('...');
 
 	const handleChange = (e: React.SyntheticEvent): void => {
 		const target = e.target as HTMLInputElement;
@@ -15,21 +16,34 @@ export default function SigningIn (){
 		}
 		
 	};
+
+	const clickInput = (element as HTMLInputElement): void => {
+		if(element.name === 'email') {
+			setEmail('');
+		} else if (element.name === 'password') {
+			setEmail('');
+		}
+	};
+
+	const emailInput = document.getElementById('emailInputSI') as HTMLInputElement;
+	emailInput.addEventListener('click', clickInput(emailInput));
+
 	return(
 		<div className='signInBlock'>
-			<h2>Please, sign in</h2>
-			<div className='signInBlock_emailBlock'>
-				<label>
+			<h2 className='signInBlock_header'>Please, sign in</h2>
+			<div className='inputBlock'>
+				<label className='inputBlock_label'>
                     Email
-					<input className={email === '' ? 'signInBlock_input' : 'signInBlock_input__Warning'} type='text' name= 'email' id='emailInputSI' value={email} onChange={handleChange}></input>
+					<input className={email !== '' ? 'inputBlock_input inputBlock_input__email' : 'inputBlock_input__Warning'} type='text' name= 'email' id='emailInputSI' value={email} onChange={handleChange}></input>
 				</label>
 			</div>
-			<div className='signInBlock_passwordBlock'>
-				<label>
+			<div className='inputBlock'>
+				<label className='inputBlock_label'>
                     Password
-					<input className={password === '' ? 'signInBlock_input' : 'signInBlock_input__Warning'} type='text' name='password' id='passwordInputSI' value={password} onChange={handleChange}></input>
+					<input className={password !== '' ? 'inputBlock_input inputBlock_input__password' : 'inputBlock_input__Warning'} type='text' name='password' id='passwordInputSI' value={password} onChange={handleChange}></input>
 				</label>
 			</div>
+			<button className='signInBlock_button'>Submit</button>
 		</div>
 	);
 	
