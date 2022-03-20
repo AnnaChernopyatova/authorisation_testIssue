2335c290bb73c1d1f9cb66037bdddc41e5c08b42
AnnaChernopyatova <anna.chernopyatova913@mail.ru>
2022-03-20 22:19:13 +0300

    Complete signingIn component

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import SigningIn from './SigningIn';
 import './styles/mainPage.css';
 
 export default function MainPage () {
+	const [authorised, setAuthorised] = React.useState<boolean>();
+
+	const handleChange = () => {
+		setAuthorised(!authorised);
+	};
+
+	useEffect(()=> {
+		const data = localStorage.getItem('authorisedUser');
+		if(!data){
+			setAuthorised(false);
+		} else {
+			setAuthorised(true);
+		}
+	}, []);
+
 	return(
 		<div className='wholePage'>
 			<Header></Header>
-			<SigningIn></SigningIn>
+			{
+				authorised ? <></> :<SigningIn handleChangeAuthorisation={handleChange}></SigningIn>
+			}
+			
 		</div>
 	);
 }
--- src/components/SigningIn.tsx
+++ src/components/SigningIn.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/signingIn.css';
 
-export default function SigningIn (){
+interface authorisedProps{
+	handleChangeAuthorisation: () => void
+}
+
+export default function SigningIn ({handleChangeAuthorisation}: authorisedProps){
 	const [email, setEmail] = React.useState<string>('...');
 	const [password, setPassword] = React.useState<string>('...');
+
+	let emailInput :HTMLInputElement;
+	let passwordInput :HTMLInputElement;
+
+	useEffect(() => {
+		const getEmailInput = document.getElementById('emailInputSI') as HTMLInputElement;
+		if(getEmailInput !== null){
+			emailInput = getEmailInput;
+			emailInput.addEventListener('click', clickInput);
+		} 
+
+		const getPasswordInput = document.getElementById('passwordInputSI') as HTMLInputElement;
+		if(getPasswordInput !== null){
+			passwordInput = getPasswordInput;
+			passwordInput.addEventListener('click', clickInput);
+		} 
+	}, []);
 
 	const handleChange = (e: React.SyntheticEvent): void => {
 		const target = e.target as HTMLInputElement;
@@ -28,11 +49,23 @@ export default function SigningIn (){
 		}
 	}
 
-	const emailInput = document.getElementById('emailInputSI') as HTMLInputElement;
-	emailInput.addEventListener('click', clickInput);
+	const saveChanges = () => {
+		if (!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) || password.length < 6) {
+			if(!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+				document.getElementById('unvalidEmailWarning')?.classList.remove('inputBlock_warning__unactive');
+				document.getElementById('unvalidEmailWarning')?.classList.add('inputBlock_warning');
+			}
+			if(password.length < 6) {
+				document.getElementById('unvalidPasswordWarning')?.classList.remove('inputBlock_warning__unactive');
+				document.getElementById('unvalidPasswordWarning')?.classList.add('inputBlock_warning');
+			}
+			return;
+		}
 
-	const passwordInput = document.getElementById('passwordInputSI') as HTMLInputElement;
-	passwordInput.addEventListener('click', clickInput);
+		localStorage.setItem('authorisedUser', JSON.stringify(`email:${email}, password:${password}`));
+		handleChangeAuthorisation();
+	} ;
+
 
 	return(
 		<div className='signInBlock'>
@@ -40,16 +73,20 @@ export default function SigningIn (){
 			<div className='inputBlock'>
 				<label className='inputBlock_label'>
                     Email
-					<input className={email !== '' ? 'inputBlock_input inputBlock_input__email' : 'inputBlock_input__Warning'} type='text' name= 'email' id='emailInputSI' value={email} onChange={handleChange}></input>
+					<input className={email !== '' ? 'inputBlock_input inputBlock_input__email' : 'inputBlock_input__warning inputBlock_input__email'} type='text' name= 'email' id='emailInputSI' value={email} onChange={handleChange}></input>
+					<span className={email === '' ? 'inputBlock_warning' : 'inputBlock_warning__unactive'}>This field can not be empty</span>
+					<span className='inputBlock_warning__unactive' id='unvalidEmailWarning'>Please, write a valid email</span>
 				</label>
 			</div>
 			<div className='inputBlock'>
 				<label className='inputBlock_label'>
                     Password
-					<input className={password !== '' ? 'inputBlock_input inputBlock_input__password' : 'inputBlock_input__Warning'} type='text' name='password' id='passwordInputSI' value={password} onChange={handleChange}></input>
+					<input className={password !== '' ? 'inputBlock_input inputBlock_input__password' : 'inputBlock_input__warning inputBlock_input__password'} type='text' name='password' id='passwordInputSI' value={password} onChange={handleChange}></input>
+					<span className={password === '' ? 'inputBlock_warning' : 'inputBlock_warning__unactive'}>This field can not be empty</span>
+					<span className='inputBlock_warning__unactive' id='unvalidPasswordWarning'>Password must be at least 6 symbols</span>
 				</label>
 			</div>
-			<button className='signInBlock_button'>Submit</button>
+			<button className='signInBlock_button' onClick={saveChanges}>Submit</button>
 		</div>
 	);
 	
